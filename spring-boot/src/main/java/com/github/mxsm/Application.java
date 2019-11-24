1ab2e3844f0b75721708e6eb99d71c3f76ba9c85
mxsm <ljbmxsm@gmail.com>
2019-11-24 11:56:07 +0800

    修改SpringBoot的启动方式

--- spring-boot/src/main/java/com/github/mxsm/Application.java
+++ spring-boot/src/main/java/com/github/mxsm/Application.java
@@ -1,21 +1,11 @@
 package com.github.mxsm;
 
-import com.github.mxsm.bean.WarpperBean;
-import com.github.mxsm.processor.MxsmPostProcessor;
+import com.github.mxsm.config.SpringBootStrap;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
-        /*ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.github.mxsm");
-
-        WarpperBean warpperBean = applicationContext.getBean(WarpperBean.class);
-        warpperBean.print();
-        System.out.println(warpperBean.toString());*/
+        SpringApplication.run(SpringBootStrap.class,args);
     }
 }
---
+++ spring-boot/src/main/java/com/github/mxsm/config/SpringBootStrap.java
@@ -0,0 +1,12 @@
+package com.github.mxsm.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mxsm
+ * @Date 2019/11/24 11:52 description:
+ */
+@SpringBootApplication
+public class SpringBootStrap {
+
+}
