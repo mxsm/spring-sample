598f694bd5eb79f03cfc888f5682c0e985322d51
mxsm <ljbmxsm@gmail.com>
2020-05-17 19:19:10 +0800

    添加

---
+++ mxsm-archetype/spring-boot-archetype/src/main/resources/archetype-resources/src/main/java/SpringApplicationBootstrap.java
@@ -0,0 +1,12 @@
+package $package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationBootstrap{
+
+    public static void main( String[] args ){
+        SpringApplication.run(SpringApplicationBootstrap.class, args)
+    }
+}
