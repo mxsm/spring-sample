03e792abff91a42ed04527f294527c4f03e2891c
mxsm <ljbmxsm@gmail.com>
2022-01-20 22:04:24 +0800

    add example

---
+++ namespace-handler/src/main/java/com/github/mxsm/enable/EnableLogConfig.java
@@ -0,0 +1,15 @@
+package com.github.mxsm.enable;
+
+import com.github.mxsm.log.EnableLog;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mxsm
+ * @date 2022/1/20 21:39
+ * @Since 1.0.0
+ */
+@EnableLog
+@Configuration
+public class EnableLogConfig {
+
+}
